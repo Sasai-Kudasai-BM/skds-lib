3de40324749f06b8eb1f6986e361fbc2595628ae
Sasai-Kudasai-BM <68680203+Sasai-Kudasai-BM@users.noreply.github.com>
2024-08-29 10:15:51 +0700

    bench

---
+++ src/net/skds/lib/benchmark/Benchmark.java
@@ -0,0 +1,52 @@
+package net.skds.lib.benchmark;
+
+import net.skds.lib.utils.SKDSUtils;
+
+public abstract class Benchmark {
+
+	private final int warmup;
+	private final int iterations;
+
+	private double resultWarmup;
+	private double resultBench;
+
+	protected Benchmark(int warmup, int iterations) {
+		this.warmup = warmup;
+		this.iterations = iterations;
+	}
+
+
+	public final void run() {
+		run(SKDSUtils.EMPTY_RUNNABLE);
+	}
+
+	public final void run(Runnable onFinish) {
+		prepare();
+		long t = System.nanoTime();
+
+		for (int i = 0; i < warmup; i++) {
+			bench();
+		}
+		resultWarmup = (double) (System.nanoTime() - t);
+		//System.gc();
+		t = System.nanoTime();
+		for (int i = 0; i < iterations; i++) {
+			bench();
+		}
+		resultBench = (double) (System.nanoTime() - t);
+		onFinish.run();
+	}
+
+	public final String result() {
+		return "======== Bench =========\n" +
+				"Warmup: " + (resultWarmup / 1000_000) + " ms\n" +
+				((resultWarmup / 1000) / warmup) + " us/op\n" +
+				"Bench: " + (resultBench / 1000_000) + " ms\n" +
+				((resultBench / 1000) / iterations) + " us/op\n" +
+				"======= Bench end =======";
+	}
+
+	public abstract void prepare();
+
+	public abstract void bench();
+}
