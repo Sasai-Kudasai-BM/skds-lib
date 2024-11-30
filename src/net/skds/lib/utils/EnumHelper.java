55a0550d0208fefa795bd322887e81a6bf5cf2b7
Sasai-Kudasai-BM <68680203+Sasai-Kudasai-BM@users.noreply.github.com>
2024-11-30 20:49:33 +0700

    fixes

--- src/net/skds/lib/utils/EnumHelper.java
+++ src/net/skds/lib/utils/EnumHelper.java
@@ -3,24 +3,34 @@ package net.skds.lib.utils;
 import net.libmerge.Lib2Merge;
 
 @Lib2Merge
-public interface EnumUtils<T extends Enum<T>> {
+public interface EnumHelper<T extends Enum<T>> {
 
 	@SuppressWarnings("unchecked")
 	private Enum<T> asEnum() {
 		return (Enum<T>) this;
 	}
+
 	@SuppressWarnings("unchecked")
 	private T[] enumValues() {
-		return (T[])asEnum().getClass().getEnumConstants();
+		return (T[]) asEnum().getClass().getEnumConstants();
 	}
 
 	default boolean lEqual(T other) {
 		return asEnum().ordinal() <= other.ordinal();
 	}
 
+	default boolean less(T other) {
+		return asEnum().ordinal() < other.ordinal();
+	}
+
 	default boolean gEqual(T other) {
 		return asEnum().ordinal() >= other.ordinal();
 	}
+
+	default boolean greater(T other) {
+		return asEnum().ordinal() > other.ordinal();
+	}
+
 	default T nextEnumValue() {
 		return ArrayUtils.loop(asEnum().ordinal() + 1, enumValues());
 	}
