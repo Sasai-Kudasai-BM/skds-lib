b32ab2fa3b0a0ba9c2f9a4eb2771c6dcb44ca572
Userhunt <dimburov@mail.ru>
2025-03-05 14:49:01 +0300

    Shapes - canCollide Fix Vec3PID

--- src/net/skds/lib2/mat/pid/Vec3PID.java
+++ src/net/skds/lib2/mat/pid/Vec3PID.java
@@ -23,8 +23,8 @@ public final class Vec3PID extends AbstractPID {
 	public Vec3 loop(Vec3 in) {
 		Vec3 d = in.sub(this.lastD).scale(this.d);
 		this.lastD = in;
-		Vec3 i = this.sumI.add(in).scale(this.i);
-
+		this.sumI = this.sumI.add(in);
+		Vec3 i = sumI.scale(this.i);
 		return in.scale(p).add(d).add(i);
 	}
 
--- src/net/skds/lib2/shapes/CollisionContext.java
+++ src/net/skds/lib2/shapes/CollisionContext.java
@@ -5,6 +5,10 @@ import net.skds.lib2.mat.vec3.Vec3;
 public interface CollisionContext {
 	int compare(Collision a, Collision b, Vec3 velocity);
 
+	default boolean canCollide(Shape shape) {
+		return true;
+	}
+
 	public static final CollisionContext DEFAULT = (a, b, v) -> {
 		return a.compareTo(b);
 	};
