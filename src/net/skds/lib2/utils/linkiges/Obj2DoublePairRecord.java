d4fc1529249cac0186e12dffac341826ac85b72c
Userhunt <dimburov@mail.ru>
2025-06-04 20:17:23 +0300

    Arsenal, Inventory

--- src/net/skds/lib2/utils/linkiges/Obj2DoublePairRecord.java
+++ src/net/skds/lib2/utils/linkiges/Obj2DoublePairRecord.java
@@ -1,7 +1,7 @@
 package net.skds.lib2.utils.linkiges;
 
 public record Obj2DoublePairRecord<O>(O objectValue, double doubleValue) implements Obj2DoublePair<O> {
-	public Obj2DoublePairRecord(double floatValue, O objectValue) {
-		this(objectValue, floatValue);
+	public Obj2DoublePairRecord(double doubleValue, O objectValue) {
+		this(objectValue, doubleValue);
 	}
 }
--- src/net/skds/lib2/utils/linkiges/Obj2IntPairRecord.java
+++ src/net/skds/lib2/utils/linkiges/Obj2IntPairRecord.java
@@ -1,7 +1,7 @@
 package net.skds.lib2.utils.linkiges;
 
 public record Obj2IntPairRecord<O>(O objectValue, int intValue) implements Obj2IntPair<O> {
-	public Obj2IntPairRecord(int floatValue, O objectValue) {
-		this(objectValue, floatValue);
+	public Obj2IntPairRecord(int intValue, O objectValue) {
+		this(objectValue, intValue);
 	}
 }
