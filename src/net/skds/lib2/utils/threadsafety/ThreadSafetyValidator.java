a420c747423e3d5af14eac4a668f5eb33d28451e
Sasai-Kudasai-BM <68680203+Sasai-Kudasai-BM@users.noreply.github.com>
2025-07-23 19:16:30 +0700

    +++++++

--- src/net/skds/lib2/utils/threadsafety/ThreadSafetyValidator.java
+++ src/net/skds/lib2/utils/threadsafety/ThreadSafetyValidator.java
@@ -33,12 +33,12 @@ public class ThreadSafetyValidator {
 
 	public static void ensureNotForbidden(Object marker) {
 		if (ENTRIES.get().forbidden.contains(marker))
-			throw new WrongThreadException("Safety check failed for thread \"" + Thread.currentThread() + "\" due to forbidden marker + \"" + marker + "\"");
+			throw new WrongThreadException("Safety check failed for thread \"" + Thread.currentThread() + "\" due to forbidden marker \"" + marker + "\"");
 	}
 
 	public static void ensurePermitted(Object marker) {
 		if (!ENTRIES.get().permitted.contains(marker))
-			throw new WrongThreadException("Safety check failed for thread \"" + Thread.currentThread() + "\" due to lack of permitted marker + \"" + marker + "\"");
+			throw new WrongThreadException("Safety check failed for thread \"" + Thread.currentThread() + "\" due to lack of permitted marker \"" + marker + "\"");
 	}
 
 	private record Entry(Set<Object> forbidden, Set<Object> permitted) {
