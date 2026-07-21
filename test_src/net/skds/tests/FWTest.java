c7799bf1017ac0b62d0c1ab375769a209857cbac
Sasai-Kudasai-BM <barmaulminemc@gmail.com>
2026-07-21 20:25:53 +0700

    ForeignWrapper tests

--- test_src/net/skds/tests/FWTest.java
+++ test_src/net/skds/tests/FWTest.java
@@ -5,41 +5,41 @@ import net.skds.lib2.natives.wrapper.ForeignWrapper;
 import net.skds.lib2.natives.wrapper.annotation.LibraryInfo;
 import net.skds.lib2.natives.wrapper.annotation.LibraryName;
 import net.skds.lib2.utils.SKDSUtils;
-import net.skds.lib2.utils.logger.SKDSLogger;
 
 import java.lang.foreign.Arena;
 
 import static net.skds.lib2.natives.MemoryAccess.getInt;
 
-public class FWTest implements Runnable {
+public class FWTest {
 
 	static void main() {
-		SKDSLogger.replaceOuts();
-
 		System.out.println("start");
+
 		GLTest glTest = ForeignWrapper.wrap(GLTest.class);
 		System.out.println(glTest);
-
 		System.out.println(glTest.glGetError());
 		System.out.println(glTest.glGetString(0x1F02));
 		glTest.glClearColor(0, 0, 0, 0);
-
 		System.out.println("end GL");
+		System.out.println("hash " + glTest.hashCode());
 
 		VKTest vkTest = ForeignWrapper.wrap(VKTest.class);
-
 		try (Arena arena = Arena.ofConfined()) {
 			long lPtr0 = MemoryAccess.alloc8(arena, 1);
 			System.out.println("err: " + vkTest.vkEnumerateInstanceLayerProperties(lPtr0, 0));
-			int size = getInt(lPtr0);
-			System.out.println("size: " + size);
+			System.out.println("size: " + getInt(lPtr0));
+			System.out.println("err: " + vkTest.vkEnumerateInstanceExtensionProperties(0, lPtr0, 0));
+			System.out.println("size: " + getInt(lPtr0));
 		}
 		System.out.println("end VK");
-	}
 
-	@Override
-	public void run() {
-		System.out.println();
+		Null nullTest = ForeignWrapper.wrap(Null.class);
+		try {
+			nullTest.who(1, 2);
+		} catch (Exception e) {
+			System.out.println(e.getCause() + "");
+		}
+		System.out.println("end");
 	}
 
 	@LibraryInfo({
@@ -62,5 +62,12 @@ public class FWTest implements Runnable {
 	public interface VKTest {
 
 		int vkEnumerateInstanceLayerProperties(long pPropertyCount, long pProperties);
+
+		int vkEnumerateInstanceExtensionProperties(long pLayerName, long pPropertyCount, long pProperties);
+	}
+
+	public interface Null {
+
+		int who(long a, long b);
 	}
 }
